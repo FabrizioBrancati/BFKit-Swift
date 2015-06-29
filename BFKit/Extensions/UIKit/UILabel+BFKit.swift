4b219af9bd6c37f2afaf91594e2b86bae93f47ef
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-06-29 13:38:16 +0200

    Implemented UILabel extension

---
+++ BFKit/Extensions/UIKit/UILabel+BFKit.swift
@@ -0,0 +1,50 @@
+//
+//  UILabel+BFKit.swift
+//  BFKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Fabrizio Brancati. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+import UIKit
+
+extension UILabel
+{
+    // MARK: - Init functions -
+    
+    convenience init(frame: CGRect, text: String, font: FontName, size: CGFloat, color: UIColor, alignment: NSTextAlignment, lines: Int)
+    {
+        self.init(frame: frame, text: text, font: font, size: size, color: color, alignment: alignment, lines: lines, shadowColor: UIColor.clearColor())
+    }
+    
+    convenience init(frame: CGRect, text: String, font: FontName, size: CGFloat, color: UIColor, alignment: NSTextAlignment, lines: Int, shadowColor: UIColor)
+    {
+        self.init(frame: frame)
+        self.font = UIFont(fontName: font, size: size)
+        self.text = text
+        self.backgroundColor = UIColor.clearColor()
+        self.textColor = color
+        self.textAlignment = alignment
+        self.numberOfLines = lines
+        self.shadowColor = shadowColor
+    }
+}
