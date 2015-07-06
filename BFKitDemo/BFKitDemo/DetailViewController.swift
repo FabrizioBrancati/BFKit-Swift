5543041d3b3aca6b0e98f1aa7f7efb2e416231ac
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-07-06 20:45:32 +0200

    Implemented other classes in DEMO App

--- BFKitDemo/BFKitDemo/DetailViewController.swift
+++ BFKitDemo/BFKitDemo/DetailViewController.swift
@@ -166,6 +166,146 @@ class DetailViewController: UIViewController
             otherFontButton.setTitleFont(.Chalkduster, size: 17)
             otherFontButton.setTitleColor(UIColor(red: 0.9218, green: 0.565, blue: 0.139, alpha: 1.0))
             scrollView.addSubview(otherFontButton)
+        case .UIColor:
+            scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT - 120)
+            
+            let hexStringColor: UIView = UIView(frame: CGRectMake(20, 80, SCREEN_WIDTH - 40, 44))
+            hexStringColor.backgroundColor = UIColor(hex: "#36a9e0")
+            let hexStringColorLabel: UILabel = UILabel(frame: CGRectMake(0, 0, SCREEN_WIDTH - 40, 44), text: "HEX String Color", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            hexStringColor.addSubview(hexStringColorLabel)
+            self.view.addSubview(hexStringColor)
+            
+            let hexColor: UIView = UIView(frame: CGRectMake(20, 140, SCREEN_WIDTH - 40, 44))
+            hexColor.backgroundColor = UIColor(hex: 0xEB9023)
+            let hexColorLabel: UILabel = UILabel(frame: CGRectMake(0, 0, SCREEN_WIDTH - 40, 44), text: "HEX Color", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            hexColor.addSubview(hexColorLabel)
+            self.view.addSubview(hexColor)
+            
+            let randomColor: UIView = UIView(frame: CGRectMake(20, 200, SCREEN_WIDTH - 40, 44))
+            randomColor.backgroundColor = UIColor.randomColor()
+            let randomColorLabel: UILabel = UILabel(frame: CGRectMake(0, 0, SCREEN_WIDTH - 40, 44), text: "Random Color", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            randomColor.addSubview(randomColorLabel)
+            self.view.addSubview(randomColor)
+        case .UIDevice:
+            scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT - 120)
+            
+            var deviceInfoString: String = ""
+            
+            if UIDevice.isiPhone()
+            {
+                deviceInfoString += "Device: iPhone\n"
+            }
+            else if UIDevice.isiPad()
+            {
+                deviceInfoString += "Device: iPad\n"
+            }
+            else if UIDevice.isiPod()
+            {
+                deviceInfoString += "Device: iPod\n"
+            }
+            else if UIDevice.isSimulator()
+            {
+                deviceInfoString += "Device: Simulator\n"
+            }
+            
+            if UIDevice.isRetina()
+            {
+                deviceInfoString += "Retina: Yes\n"
+            }
+            else
+            {
+                deviceInfoString += "Retina: No\n"
+            }
+            
+            if UIDevice.isRetinaHD()
+            {
+                deviceInfoString += "Retina HD: Yes\n"
+            }
+            else
+            {
+                deviceInfoString += "Retina HD: No\n"
+            }
+            
+            deviceInfoString += "iOS Version: \(UIDevice.iOSVersion())\n"
+            
+            deviceInfoString += "RAM Size: \(UIDevice.ramSize() / 1024 / 1024) MB\n"
+            
+            deviceInfoString += "Unique Identifier: \(UIDevice.uniqueIdentifier())\n"
+            
+            let deviceInfoLabel: UILabel = UILabel(frame: CGRectMake(20, 20, SCREEN_WIDTH - 40, 200), text: deviceInfoString, font: .HelveticaNeue, size: 16, color: UIColor.blackColor(), alignment: .Left, lines: 8)
+            scrollView.addSubview(deviceInfoLabel)
+        case .UIFont:
+            scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT - 120)
+            
+            UIFont.allFamilyAndFonts()
+            
+            let fontLabel: UILabel = UILabel(frame: CGRectMake(20, 20, SCREEN_WIDTH - 40, 25), text: "Use a font with just an ENUM ^_^", font: .NoteworthyLight, size: 16, color: UIColor.blackColor(), alignment: .Left, lines: 1)
+            scrollView.addSubview(fontLabel)
+            
+            let otherFontLabel: UILabel = UILabel(frame: CGRectMake(20, 60, SCREEN_WIDTH - 40, 25), text: "It's awesome!", font: .SuperclarendonBold, size: 16, color: UIColor.blackColor(), alignment: .Left, lines: 1)
+            scrollView.addSubview(otherFontLabel)
+        case .UIImage:
+            scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, ((SCREEN_WIDTH - 40) * 10) + (20 * 11))
+            
+            let blendOverlayImage: UIImage = UIImage(named: "Logo")!.blendOverlay()
+            let blendOverlayImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: blendOverlayImage)
+            let blendOverlayLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Blend Overlay", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            blendOverlayImageView.addSubview(blendOverlayLabel)
+            scrollView.addSubview(blendOverlayImageView)
+            
+            let imageAtRectImage: UIImage = UIImage(named: "Logo")!.imageAtRect(CGRectMake(0, 0, 250, 250))
+            let imageAtRectImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: imageAtRectImage)
+            let imageAtRectLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Image At Rect", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            imageAtRectImageView.addSubview(imageAtRectLabel)
+            scrollView.addSubview(imageAtRectImageView)
+            
+            let rotatedImage: UIImage = UIImage(named: "Logo")!.imageRotatedByDegrees(90)
+            let rotatedImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: rotatedImage)
+            let rotatedLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Rotated Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            rotatedImageView.addSubview(rotatedLabel)
+            scrollView.addSubview(rotatedImageView)
+            
+            let filledImage: UIImage = UIImage(named: "Logo2")!.fillAlphaWithColor(UIColor ( red: 0.9255, green: 0.6929, blue: 0.1188, alpha: 1.0 ))
+            let filledImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: filledImage)
+            let filledLabel: UILabel = UILabel(frame: CGRectMake(0, 40, SCREEN_WIDTH - 40, 25), text: "Filled Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            filledImageView.addSubview(filledLabel)
+            scrollView.addSubview(filledImageView)
+            
+            let invertedImage: UIImage = UIImage(named: "Logo")!.invertColors()
+            let invertedImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20  + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: invertedImage)
+            let invertedLabel: UILabel = UILabel(frame: CGRectMake(0, 40, SCREEN_WIDTH - 40, 25), text: "Inverted Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            invertedImageView.addSubview(invertedLabel)
+            scrollView.addSubview(invertedImageView)
+            
+            let blurredImage: UIImage = UIImage(named: "Logo")!.blur(radius: 10)!
+            let blurredImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20 + invertedImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: blurredImage)
+            let blurredLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Blurred Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            blurredImageView.addSubview(blurredLabel)
+            scrollView.addSubview(blurredImageView)
+            
+            let bumpedImage: UIImage = UIImage(named: "Logo")!.bumpDistortionLinear(CIVector(x: SCREEN_WIDTH - 40, y:SCREEN_WIDTH - 40), radius: 100, angle: Float(M_PI), scale: 2)
+            let bumpedImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20 + invertedImageView.frame.size.height + 20 + blurredImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: bumpedImage)
+            let bumpedLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Bump Distortion Linear Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            bumpedImageView.addSubview(bumpedLabel)
+            scrollView.addSubview(bumpedImageView)
+            
+            let sepiaImage: UIImage = UIImage(named: "Logo")!.sepiaToneWithIntensity(1)
+            let sepiaImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20 + invertedImageView.frame.size.height + 20 + blurredImageView.frame.size.height + 20 + bumpedImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: sepiaImage)
+            let sepiaLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Sepia Tone Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            sepiaImageView.addSubview(sepiaLabel)
+            scrollView.addSubview(sepiaImageView)
+            
+            let colorImage: UIImage = UIImage(color: UIColor(red: 0.8784, green: 0.7106, blue: 0.0, alpha: 1.0 ))!
+            let colorImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20 + invertedImageView.frame.size.height + 20 + blurredImageView.frame.size.height + 20 + bumpedImageView.frame.size.height + 20 + sepiaImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: colorImage)
+            let colorLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Color Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            colorImageView.addSubview(colorLabel)
+            scrollView.addSubview(colorImageView)
+            
+            let dummyImage: UIImage = UIImage(dummyImageWithSizeAndColor: "100x100.#ECB11E")!
+            let dummyImageView: UIImageView = UIImageView(frame: CGRectMake(20, 20 + blendOverlayImageView.frame.size.height + 20 + imageAtRectImageView.frame.size.height + 20 + rotatedImageView.frame.size.height + 20 + filledImageView.frame.size.height + 20 + invertedImageView.frame.size.height + 20 + blurredImageView.frame.size.height + 20 + bumpedImageView.frame.size.height + 20 + sepiaImageView.frame.size.height + 20 + colorImageView.frame.size.height + 20, SCREEN_WIDTH - 40, SCREEN_WIDTH - 40), image: dummyImage)
+            let dummyLabel: UILabel = UILabel(frame: CGRectMake(0, 20, SCREEN_WIDTH - 40, 25), text: "Dummy Image", font: .HelveticaNeue, size: 16, color: UIColor.whiteColor(), alignment: .Center, lines: 1, shadowColor: UIColor.blackColor())
+            dummyImageView.addSubview(dummyLabel)
+            scrollView.addSubview(dummyImageView)
         default:
             break
         }
