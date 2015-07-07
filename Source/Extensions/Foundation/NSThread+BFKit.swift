f19ad1b1917f16291fe3c5a765351f419d4a1df3
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-07-07 18:35:29 +0200

    NSThread extension is now public and can perform selectors in background

--- Source/Extensions/Foundation/NSThread+BFKit.swift
+++ Source/Extensions/Foundation/NSThread+BFKit.swift
@@ -40,8 +40,47 @@ public func runOnMainThread(block: () -> ())
     })
 }
 
-/// This extesion adds some useful functions to NSThread
-private extension NSThread
+/**
+Runs a block in background
+
+:param: block Block to be executed
+*/
+public func runInBackground(block: () -> ())
 {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+        block()
+    }
+}
+
+/// This extesion adds some useful functions to NSThread
+public extension NSThread
+{
+    /**
+    Exectute a selector asyncronously after a delay
     
+    :param: selector Selector to be executed
+    :param: target   Target (Usually "self")
+    :param: delay    Delay to excute the selector
+    
+    :returns: Return an NSTimer who handle the execution of the selector
+    */
+    public static func callSelectorAsync(selector: Selector, target: AnyObject, delay: NSTimeInterval = 0.0) -> NSTimer
+    {
+        return NSTimer.scheduledTimerWithTimeInterval(delay, target: target, selector: selector, userInfo: nil, repeats: false)
+    }
+    
+    /**
+    Exetute a selector
+    
+    :param: selector Selector to be executed
+    :param: target   Target (Usually "self")
+    :param: object   Object to pass to the selector
+    :param: delay    Delay to excute the selector
+    */
+    public static func callSelector(selector: Selector, target: AnyObject, object: AnyObject? = nil, delay: NSTimeInterval = 0.0)
+    {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
+            NSThread.detachNewThreadSelector(selector, toTarget: target, withObject: object)
+        })
+    }
 }
