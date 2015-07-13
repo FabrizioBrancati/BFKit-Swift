7b54c77d06546a9624fd7e30316696732c00eed0
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-07-13 12:44:48 +0200

    Added UIToolbar and UIBarButtonItem extensions

---
+++ Source/Extensions/UIKit/UIBarButtonItem.swift
@@ -0,0 +1,56 @@
+//
+//  UIBarButtonItem+BFKit.swift
+//  BFKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 Fabrizio Brancati. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+import UIKit
+
+/// This extesion adds some useful functions to UIBarButtonItem
+public extension UIBarButtonItem
+{
+    /**
+    Create an UIBarButtonItem with type setted to FlexibleSpace or FixedSpace
+    
+    :param: space Must be FlexibleSpace or FixedSpace, otherwise a FlexibleSpace UIBarButtonItem will be created
+    :param: width To use only if space is setted to FixedSpace, and it will be the width of it
+    
+    :returns: Returns the created UIBarButtonItem
+    */
+    public convenience init(barButtonSpaceType space: UIBarButtonSystemItem, width: CGFloat = 0.0)
+    {
+        if space == .FixedSpace || space == .FlexibleSpace
+        {
+            self.init(barButtonSystemItem: space, target: nil, action: "")
+            if space == .FixedSpace
+            {
+                self.width = width
+            }
+        }
+        else
+        {
+            self.init(barButtonSystemItem: .FlexibleSpace, target: nil, action: "")
+        }
+    }
+}
