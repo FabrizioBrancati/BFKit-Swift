7edf673c8fde5e84bc229d8e3e4413496883d58f
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-05-09 19:24:09 +0200

    Added UIPasteboard extension

---
+++ Source/Extensions/UIKit/UIPasteboard.swift
@@ -0,0 +1,55 @@
+//
+//  UIPasteboard+BFKit.swift
+//  BFKit
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2015 - 2016 Fabrizio Brancati. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+import UIKit
+
+/// This extesion adds some useful functions to UIPasteboard
+public extension UIPasteboard {
+    // MARK: - Class functions -
+    
+    /**
+     Copy a text to the pasteboard
+     
+     - parameter text: The text to be copy to
+     */
+    public static func copyToPasteboard(text: String) {
+        UIPasteboard.generalPasteboard().string = text
+    }
+    
+    /**
+     Returns the last copied string from pasteboard
+     
+     - returns: Returns the last copied string from pasteboard
+     */
+    public static func stringFromPasteboard() -> String {
+        guard let pasteboard = UIPasteboard.generalPasteboard().string else {
+            return ""
+        }
+        
+        return pasteboard
+    }
+}
