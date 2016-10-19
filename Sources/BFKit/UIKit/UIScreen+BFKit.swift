421165a8515a520ba3a97c621e6dc36a87ee82ef
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-10-20 00:20:34 +0200

    Removed iOS 7 support

--- Sources/BFKit/UIKit/UIScreen+BFKit.swift
+++ Sources/BFKit/UIKit/UIScreen+BFKit.swift
@@ -107,10 +107,6 @@ public extension UIScreen {
     public func fixedScreenSize() -> CGSize {
         let screenSize = self.bounds.size
         
-        if NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_7_1 && UIInterfaceOrientationIsLandscape(UIApplication.shared.statusBarOrientation) {
-            return CGSize(width: screenSize.height, height: screenSize.width)
-        }
-        
         return screenSize
     }
     
