9f725ee6dde07a29949de99145bafbbdb36b73f6
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2017-01-08 18:10:06 +0100

    Adding cryptographically secure random numbers for iOS

--- Sources/BFKit/Linux/Foundation/NumberExtension.swift
+++ Sources/BFKit/Linux/Foundation/NumberExtension.swift
@@ -48,10 +48,10 @@ public func radiansToDegrees(_ radians: Float) -> Float {
 /// Create a random integer between the given range.
 ///
 /// - Parameters:
-///   - minValue: Mininum random value.
-///   - maxValue: Maxinum random value.
+///   - minValue: Mininum random value. Default is 0.
+///   - maxValue: Maxinum random value. Default is 1.
 /// - Returns: Returns the created random integer.
-public func randomInt(min minValue: Int, max maxValue: Int) -> Int {
+public func randomInt(min minValue: Int = 0, max maxValue: Int = 100) -> Int {
     return randomInt(range: minValue...maxValue)
 }
 
@@ -61,20 +61,7 @@ public func randomInt(min minValue: Int, max maxValue: Int) -> Int {
 /// - Parameter range: Range random value.
 /// - Returns: Returns the created random integer.
 public func randomInt(range: ClosedRange<Int>) -> Int {
-    var offset = 0
-    
-    if range.lowerBound < 0 {
-        offset = abs(range.lowerBound)
-    }
-    
-    let min = UInt32(range.lowerBound + offset)
-    let max = UInt32(range.upperBound + offset)
-    
-    #if os(Linux)
-        return Int(Int(min) + Int(abs(Int.random())) % Int(max - min)) - offset
-    #else
-        return Int(min + arc4random_uniform(max - min)) - offset
-    #endif
+    return Int(randomFloat(range: Float(range.lowerBound)...Float(range.upperBound)))
 }
 
 /// Create a random float between the given range.
@@ -84,24 +71,30 @@ public func randomInt(range: ClosedRange<Int>) -> Int {
 ///   - maxValue: Maxinum random value. Default is 1.
 /// - Returns: Returns the created random float.
 public func randomFloat(min minValue: Float = 0, max maxValue: Float = 1) -> Float {
-    #if os(Linux)
-        let random = abs(Float.random())
-    #else
-        let random = Float(arc4random()) / Float(UINT32_MAX)
-    #endif
-    return random * abs(minValue - maxValue) + min(minValue, maxValue)
+    return randomFloat(range: minValue...maxValue)
+}
+
+/// Create a random float between the given range.
+/// Example: randomFloat(-500...100).
+///
+/// - Parameter range: Range random value.
+/// - Returns: Returns the created random float.
+public func randomFloat(range: ClosedRange<Float>) -> Float {
+    return Float(range.upperBound - range.lowerBound) * abs(Float.random()) + Float(range.lowerBound)
 }
 
 // MARK: - Randomizer struct
 
-#if os(Linux)
-    /// Produces great cryptographically random numbers.
-    private struct Randomizer {
-        /// /dev/urandom file.
+/// Produces great cryptographically random numbers.
+private struct Randomizer {
+    #if os(Linux)
+        /// /dev/urandom file reader.
         static let file = fopen("/dev/urandom", "r")!
-        /// Random queue.
-        static let queue = DispatchQueue(label: "random")
-        
+    #endif
+    /// Random queue.
+    static let queue = DispatchQueue(label: "random")
+    
+    #if os(Linux)
         /// Get a random number of a given capacity.
         ///
         /// - Parameter count: Byte count.
@@ -115,29 +108,46 @@ public func randomFloat(min minValue: Float = 0, max maxValue: Float = 1) -> Flo
             _ = queue.sync {
                 fgets(data, Int32(capacity), file)
             }
+    
             return Array(UnsafeMutableBufferPointer(start: data, count: count))
         }
-    }
-#endif
+    #else
+        /// Get a random number of a given capacity.
+        ///
+        /// - Parameter count: Byte count.
+        /// - Returns: Return the random number.
+        static func get(count: Int) -> [UInt8] {
+            let capacity = count + 1
+            var data = UnsafeMutablePointer<UInt8>.allocate(capacity: capacity)
+            var secure: Int32 = 0
+            defer {
+                data.deallocate(capacity: capacity)
+            }
+            _ = queue.sync {
+                secure = SecRandomCopyBytes(kSecRandomDefault, capacity, data)
+            }
+            
+            return secure == 0 ? Array(UnsafeMutableBufferPointer(start: data, count: count)) : [0]
+        }
+    #endif
+}
 
 // MARK: - Extensions
 
-#if os(Linux)
-    /// This extension adds some useful function to Integer.
-    public extension Integer {
-        /// Creates a random integer number.
-        ///
-        /// - Returns: Returns the creates a random integer number.
-        static func random() -> Self {
-            let numbers = Randomizer.get(count: MemoryLayout<Self>.size)
-            return numbers.withUnsafeBufferPointer { bufferPointer in
-                return bufferPointer.baseAddress!.withMemoryRebound(to: Self.self, capacity: 1) {
-                    return $0.pointee
-                }
+/// This extension adds some useful function to Integer.
+public extension Integer {
+    /// Creates a random integer number.
+    ///
+    /// - Returns: Returns the creates a random integer number.
+    static func random() -> Self {
+        let numbers = Randomizer.get(count: MemoryLayout<Self>.size)
+        return numbers.withUnsafeBufferPointer { bufferPointer in
+            return bufferPointer.baseAddress!.withMemoryRebound(to: Self.self, capacity: 1) {
+                return $0.pointee
             }
         }
     }
-#endif
+}
 
 /// This extesion adds some useful functions to Double.
 public extension Double {
@@ -146,14 +156,12 @@ public extension Double {
         return description.characters.map { Int(String($0)) ?? 0 }
     }
     
-    #if os(Linux)
-        /// Creates a random Double number.
-        ///
-        /// - Returns: Returns the creates a random Double number.
-        static func random() -> Double {
-            return Double(Int.random()) / Double(Int.max)
-        }
-    #endif
+    /// Creates a random Double number.
+    ///
+    /// - Returns: Returns the creates a random Double number.
+    static func random() -> Double {
+        return Double(Int.random()) / Double(Int.max)
+    }
 }
 
 /// This extesion adds some useful functions to Float.
@@ -163,14 +171,12 @@ public extension Float {
         return description.characters.map { Int(String($0)) ?? 0 }
     }
     
-    #if os(Linux)
-        /// Creates a random Float number.
-        ///
-        /// - Returns: Returns the creates a random Float number.
-        static func random() -> Float {
-            return Float(Double.random())
-        }
-    #endif
+    /// Creates a random Float number.
+    ///
+    /// - Returns: Returns the creates a random Float number.
+    static func random() -> Float {
+        return Float(Double.random())
+    }
 }
 
 /// This extesion adds some useful functions to Int.
