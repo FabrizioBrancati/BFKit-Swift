df745b70fce20f9270641545de8c317cc0a28e93
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2017-07-09 20:28:44 +0200

    Minor changes

--- Sources/BFKit/Linux/Foundation/StringExtension.swift
+++ Sources/BFKit/Linux/Foundation/StringExtension.swift
@@ -649,6 +649,7 @@ public extension String {
     #endif
 }
 
+/// Infix operator `???` with NilCoalescingPrecedence.
 infix operator ???: NilCoalescingPrecedence
 
 /// Returns defaultValue if optional is nil, otherwise returns optional.
