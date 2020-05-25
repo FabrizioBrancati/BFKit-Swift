adceefbb8e19a5e4899727e60aeb9d4eecd17f0c
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2020-05-25 12:39:53 +0200

    Updating license year to 2020

--- Sources/BFKit.h
+++ Sources/BFKit.h
@@ -3,7 +3,7 @@
 //
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2015 - 2019 Fabrizio Brancati.
+//  Copyright (c) 2015 - 2020 Fabrizio Brancati.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
